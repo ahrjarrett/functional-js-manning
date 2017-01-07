3c74bb052629cc932c9f498a00c9bf5d5343caa8
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-07 11:25:34 -0700

    added 4.9 :: alternation combinator (+ notes)

---
+++ 4_9/alternation.combinator.js
@@ -0,0 +1,15 @@
+const R = require('ramda')
+
+/* alternation combinator */
+const alt = R.curry((fn1, fn2, val) => fn1(val) || fn2(val))
+
+/* notice how using curry & lambdas removes need for
+ * nesting functions that return values from their
+ * closures */
+
+/* OR defined less succinctly: */
+//const alt = function(fn1, fn2) {
+//  return function(val) {
+//    return fn1(val) || fn2(val)
+//  }
+//}
