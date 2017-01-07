4bee1daa3b0ffb901220768b48429ac669be164c
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-07 11:53:55 -0700

    added 2nd test to fork combinator in 4.10

--- 4_10/fork.combinator.js
+++ 4_10/fork.combinator.js
@@ -18,7 +18,11 @@ const fork = function(join, fn1, fn2) {
 // example usage:
 const computeAvgGrade =
   fork(R.divide, R.sum, R.length)
+console.log(computeAvgGrade([76, 92, 87])) //-> 85
 
-console.log(computeAvgGrade([76, 92, 87])) // -> 85
+// eqMedianAverage computes whether median and avg are the same
+const eqMedianAverage = fork(R.equals, R.median, R.mean)
+console.log(eqMedianAverage([80, 90, 100])) //-> True
+console.log(eqMedianAverage([80, 91, 100])) //-> False
 
 module.exports = fork
