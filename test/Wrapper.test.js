81989d0078c065a0ce74e6e369139683ca4247cd
Andrew Jarrett <ahrjarrett@gmail.com>
2017-01-07 22:03:12 -0700

    Wrapper tests are green

--- test/Wrapper.test.js
+++ test/Wrapper.test.js
@@ -2,30 +2,29 @@ const R = require('ramda')
 const Wrapper = require('../Wrapper')
 const wrap = (val) => new Wrapper(val)
 
-describe('', () => {
-  R.identity
+describe('wrap:', () => {
+  const wrappedValue = wrap('Prof Bros is pretty rad.')
+
+  it('puts stuff in a box', () => {
+    wrappedValue.map(R.identity).should.eql('Prof Bros is pretty rad.')
+  })
 })
 
+describe('map:', () => {
+  const wrappedValue = wrap('Prof Bros ARE pretty rad...')
 
-/* remove crufty stuff below!
- * maybe add a notes directory to put this in?
- */
+  it('performs an operation on the stuff in the box', () => {
+    wrappedValue.map(R.toUpper).should.eql('PROF BROS ARE PRETTY RAD...')
+  })
+})
 
-// testing...
-const wrappedValue = wrap('Prof Bros is pretty rad.')
-console.log(wrappedValue.map(R.identity)) //-> 'Prof Bros is pretty rad.'
-wrappedValue.map(console.log) //-> 'Prof Bros is pretty rad.'
-// notice how the two above are A -> B, then B -> A
+describe('fmap:', () => {
+  const plus = R.curry((a, b) => a + b)
+  const plus3 = plus(3)
+  const two = wrap(2)
+  const five = two.fmap(plus3) //-> Wrapper(5)
 
-console.log(wrappedValue.map(R.toUpper)) //-> 'PROS BROS IS PRETTY RAD.'
-
-
-// moar tests:
-const plus = R.curry((a, b) => a + b)
-const plus3 = plus(3)
-
-const two = wrap(2)
-const five = two.fmap(plus3) //-> Wrapper(5)
-console.log(five.map(R.identity)) //-> 5
-
-
+  it('shares functionality of map with Wrapper instances', () => {
+    five.map(R.identity).should.eql(5)
+  })
+})
