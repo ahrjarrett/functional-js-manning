90c0b171581803eec634d2ae7658467b32a927d6
Andrew Jarrett <ahrjarrett@gmail.com>
2017-02-01 01:43:54 -0700

    annotated fork.js

--- src/fork.js
+++ src/fork.js
@@ -1,5 +1,11 @@
 const R = require('ramda')
 
+/*
+ * the key to using fork correctly is understanding the join param
+ * and how it applies the remaining two functions
+ */
+
+/* fork : Function, Function, Function -> Value */
 const fork = function(join, fn1, fn2) {
   return function(val) {
     return join(fn1(val), fn2(val))
@@ -7,4 +13,3 @@ const fork = function(join, fn1, fn2) {
 }
 
 module.exports = fork
-
